cb84395db6ff70d271d1900a34ab03ed26ae3bcd
AnarBabazade <babazadeanar94@gmail.com>
2024-01-23 00:46:44 +0400

    innersial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1.HOME WORK
+
+// 1-ci variant
+
+// let year=prompt("dogum ilinizi qeyd edin")
+// let month=prompt("dogum ayinizi qeyd edin")
+// let day =prompt("dogum gununuzu qeyd edin")
+// let birthday=year+"/"+month+"/"+day
+// console.log(birthday);
+
+
+
+// 2-ci variant
+
+// let date=prompt("xxxx-xx-xx formatinda ayin tarixini qeyd edin")
+// const changeFormat=(date)=>{
+//     let result=date.split("")
+//     result[4]="/"
+//     result[7]="/"
+//     let result2=result.join("")  
+//     let result3=result2.split("/").reverse().join("/")
+      
+//     console.log(result3);
+// }
+// changeFormat(date)
+
+// qeyd:bu tapsiriqda 1 ci variyant ile userlere rahat shekilde tarixi yazmagi ve mene istediyim formatda gelmeyini 
+// elde etdim.Lakin 2 ci variyantda wert qoymaq istedim ki user eger prompta agzini geleni ve ya qeyri standart formatda  
+// yazarsa alert cixsin ancaq alindira bilmedim.
+
+
+
+// 2.HOMEWORK
+
+// const users = [
+//     { name: "ANAR", salary: 1500 },
+//     { name: "CELIL", salary: 1300 },
+//     { name: "NECI", salary: 1000 },
+//     { name: "YUSIF", salary: 400 },
+//     { name: "nemet", salary: 10000 },
+
+
+//   ];
+//   const totalSalary = users.reduce((total, user) => total+user.salary,0);
+//   console.log("Total Salary:", totalSalary);
+
+
+
+
+
+// 3.HOME WORK
+
+
+// let a = [1, 2, 3, 4, 5];
+// let b = [1, 2, 3, 4, 5, 6];
+// let totalA=a.reduce((total,value)=>total+value,0)
+// let totalB=b.reduce((total,value)=>total+value,0)
+// console.log(totalB-totalA);
+
+// qeyd:bu tapsiriqda eyer ferqli reqemler bir nece dene olarsa onlari nece consola cixardacagimi tapa bilmedim
+
+
+
